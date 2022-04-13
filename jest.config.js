cc3ae5c1a244597d81d619b90966974d027ca008
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-04-12 22:07:39 -0300

    feat: configure jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  collectCoverage: true,
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
