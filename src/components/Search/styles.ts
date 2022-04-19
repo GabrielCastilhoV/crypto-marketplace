1e4e40ed12743c050ebe6b7ef016923023cb49bd
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2022-04-19 11:44:55 -0300

    style: add focus within

--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -3,8 +3,8 @@ import { styled } from 'styles/stitches.config'
 export const Wrapper = styled('label', {
   h: 40,
   borderRadius: '$md',
-  background: 'rgba(239, 239, 255, 0.2)',
-  padding: '$2 $3',
+  bg: 'rgba(239, 239, 255, 0.2)',
+  px: '$3',
   m: '$3 0 $5 0',
 
   display: 'flex',
@@ -14,11 +14,16 @@ export const Wrapper = styled('label', {
   svg: {
     w: 22,
     h: 22
+  },
+
+  '&:focus-within': {
+    boxS: '#CF7AFF 0px 2px 2px 0px;'
   }
 })
 
 export const Input = styled('input', {
   w: '100%',
+  h: '100%',
   bg: 'none',
   border: 'none',
   outline: 'none',
